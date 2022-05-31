f122b58620b6c55d2c520607e8340016eeecaeea
Jason Lengstorf <jason@lengstorf.com>
2022-05-31 13:46:59 -0700

    fix: use HTML rewrite vs. CSS because order doesn’t affect screen readers

--- netlify/edge-functions/personalize.ts
+++ netlify/edge-functions/personalize.ts
@@ -5,12 +5,33 @@ import {
 } from 'https://ghuc.cc/worker-tools/html-rewriter/index.ts';
 import { getScoreFromCookie } from './utils/cookies.ts';
 
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  imageSrc: string;
+  imageAlt: string;
+  category: 'food' | 'corgis';
+  link: string;
+};
+
 export default async (_request: Request, context: Context) => {
   const score = getScoreFromCookie(context);
 
   if (score.food === 0 && score.corgis === 0) {
     return;
   }
+
+  const endpoint = new URL(Deno.env.get('URL') || 'http://localhost:8888');
+  endpoint.pathname = '/api/products';
+
+  const res = await fetch(endpoint.toString());
+
+  if (!res.ok) {
+    return;
+  }
+
+  const products = await res.json();
 
   let prioritize = 'none';
   if (score.food > score.corgis) {
@@ -21,10 +42,37 @@ export default async (_request: Request, context: Context) => {
     prioritize = 'corgis';
   }
 
+  if (prioritize === 'none') {
+    return;
+  }
+
+  const sorted = products.sort((a: Product) =>
+    a.category === prioritize ? -1 : 1,
+  );
+  const sortedMarkup = sorted.map((product: Product) => {
+    return `
+      <div class="product" data-category="${product.category}">
+        <img
+          src="${product.imageSrc}"
+          alt="${product.imageAlt}"
+        />
+        <h2>${product.name}</h2>
+        <p>
+          ${product.description}
+        </p>
+        <a href="${
+          product.link
+        }">Shop All ${product.category.toLocaleUpperCase()}</a>
+      </div>
+    `;
+  });
+
+  console.log(sortedMarkup);
+
   return new HTMLRewriter()
     .on('.products', {
       element(element: Element) {
-        element.setAttribute('data-prioritize', prioritize);
+        element.setInnerContent(sortedMarkup.join(''), { html: true });
       },
     })
     .transform(await context.next());
---
+++ netlify/functions/products.ts
@@ -0,0 +1,57 @@
+import { builder, Handler } from '@netlify/functions';
+import fetch from 'node-fetch';
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  imageSrc: string;
+  imageAlt: string;
+  category: 'food' | 'corgis';
+  link: string;
+};
+
+export const handler: Handler = builder(async () => {
+  const res = await fetch(
+    'https://zero-js-personalized.hasura.app/v1/graphql',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+        query AllProducts {
+          products(order_by: {id: asc}) {
+            id
+            name
+            description
+            imageSrc
+            imageAlt
+            category
+            link
+          }
+        }      
+      `,
+        variables: {},
+      }),
+    },
+  );
+
+  if (!res.ok) {
+    console.error(res);
+
+    return {
+      statusCode: 500,
+      body: 'Error loading data',
+    };
+  }
+
+  const { data } = await res.json();
+  const products = data.products as Product[];
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(products),
+  };
+});
